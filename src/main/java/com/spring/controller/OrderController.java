579f2c297ac3bd568d33095cde860cd877b7161e
harrionh <lip0crystall@arch.localdomain>
2023-07-31 20:31:21 +0300

    Ready

--- src/main/java/com/spring/controller/OrderController.java
+++ src/main/java/com/spring/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.spring.controller;
 
 import com.spring.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
